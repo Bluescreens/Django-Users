079c750ee42882463fdc4813377ecf81aae0146f
RottenCrab <ndoureliadis.aueb@gmail.com>
2017-11-15 23:03:50 +0200

    add user Profile

--- users/models.py
+++ users/models.py
@@ -1,8 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
 
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    address = models.CharField(max_length=100)
+    country = models.CharField(max_length=50)
+    birth_date = models.DateField(blank=True, null=True)
+    tel = models.CharField(max_length=14)
 
-class Test(models.Model):
-    username = models.CharField(max_length=30)
-    age = models.IntegerField()
+    @receiver(post_save, sender=User)
+    def update_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+        instance.profile.save()
